d0635f5d5ef110f9f3bc0f8cc7173f992c42d814
JefriAP <jefriap28@users.noreply.github.com>
2020-11-13 22:14:11 +0700

    update style font jumbrot

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -3,13 +3,13 @@
     <div class="text">
       <h1>HIMATIKA</h1>
       <h3 id="lead">Himpunan Mahasiswa Teknik Informatika</h3>
-      <p>UNIVERSITAS TRILOGI</p>
+      <h5>UNIVERSITAS TRILOGI</h5>
     </div>
     <div v-b-hover="handleHover">
       <b-button v-if="isHovered" variant="primary" href="#"
         >Kegiatan Terbaru</b-button
       >
-      <b-button v-else variant="outline-dark" href="#"
+      <b-button v-else variant="outline-dark" class="shadow-sm rounded" href="#"
         >Kegiatan Terbaru</b-button
       >
     </div>
@@ -23,6 +23,7 @@ export default {
 </script>
 
 <style scoped>
+/* mobile */
 #jumbot {
   background: url("../assets/img/bgUniv.jpg");
   background-size: cover;
@@ -30,11 +31,16 @@ export default {
   height: 450px;
   background-attachment: fixed;
 }
-  .text h1 {
-    font-size: 30px;
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
-    margin-top: 80px;
-  }
+.text {
+  color: white;
+  text-shadow: 2px 2px 4px #000000;
+}
+.text h1 {
+  font-size: 30px;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  margin-top: 80px;
+}
+
 /* dekstop */
 @media (min-width: 768px) {
   #jumbot {
@@ -44,8 +50,13 @@ export default {
     height: 530px;
     background-attachment: fixed;
   }
+  .text {
+    color: white;
+    text-shadow: 2px 2px 4px #000000;
+  }
+
   .text h1 {
-    font-size: 70px;
+    font-size: 80px;
     font-family: Verdana, Geneva, Tahoma, sans-serif;
     margin-top: 80px;
   }
